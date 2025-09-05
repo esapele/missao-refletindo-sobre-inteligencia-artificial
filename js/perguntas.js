12c9a5c69ba36797093093b948037d34c08abd77
esapele <159255930+esapele@users.noreply.github.com>
2025-09-05 08:04:32 -0300

    rsrsrsrs

--- js/perguntas.js
+++ js/perguntas.js
@@ -4,11 +4,17 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "Chama, adorei!",
-                afirmacao: "Logo de início ja gostou do nome. "
+                afirmacao: ["Logo de início ja gostou do nome.", 
+                    "A primeira impressão ja foi boa"
+                ],
+                proxima: 1,
             },
             {
                 texto: "Não, achei o nome estranho",
-                afirmacao: "A primeira impressão nao foi das melhores."
+                afirmacao: [ "A primeira impressão nao foi das melhores." 
+                ], 
+                proxima: 2,
+            
             }
         ]
     },
@@ -19,11 +25,14 @@ export const perguntas = [
                 texto: "amei muito boa",
                 afirmacao: [ "ao ouvir a musica achou espetacular, ja quis adicionar na playlist.",
                     "ouvindo a musica logo percebeu que era do seu estilo" 
-                ]
+                ],
+                proxima: 3,
             },
             {
                 texto: "odiei muito paia",
-                afirmacao: "ao ouvir descobriu que seu gosto musical nao é refinado o suficiente para aprecia-la."
+                afirmacao: ["ao ouvir descobriu que seu gosto musical nao é refinado o suficiente para aprecia-la."
+                ],
+                proxima: 4,
             }
         ]
     },
@@ -32,12 +41,17 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "muito é massa, achei muito diva.",
-                afirmacao: "e quando pesquisa mais sobre a cantora percebe que ela é muito altentica,e realmente aprecia o seu estilo."
+                afirmacao: [ "e quando pesquisa mais sobre a cantora percebe que ela é muito altentica,e realmente aprecia o seu estilo."
+                ],
+                proxima: 3,
             },
             {
                 texto: "esquisito...",
-                afirmacao: "e quando pesquisa mais sobre a cantora, acaba achando seu forma de se portar estranha, não consegue se identificar com o que ela canta"
-            }
+                afirmacao: [ "e quando pesquisa mais sobre a cantora, acaba achando seu forma de se portar estranha, não consegue se identificar com o que ela canta"
+                ]
+                proxima: 4,
+
+                }
         ]
     },
     {
@@ -45,11 +59,15 @@ export const perguntas = [
         alternativas: [
             {
                 texto: "digo a meu amigo para achar algo melhor para gastar seu dinheirinho.",
-                afirmacao: "chega a conclusão de que ela não vale um grande gasto de dinheiro."
+                afirmacao: [ "chega a conclusão de que ela não vale um grande gasto de dinheiro."
+                ]
+                proxima: 5,
             },
             {
                 texto: "faço de tudo para conseguir dinheiro para ir no show dela também.",
-                afirmacao: "Tornou ir a seus show um de seus objetivos principais e assim foi muito mais feliz em sua vida."
+                afirmacao: [ "Tornou ir a seus show um de seus objetivos principais e assim foi muito mais feliz em sua vida."                   
+                ]
+                proxima: 6,
             }
         ]
     },
